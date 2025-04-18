28286bea07df909baf61d7788da3e56d4c82c744
ozzi17 <oslifernandes@gmail.com>
2025-04-18 19:42:54 -0300

    corrige coisas de itp

--- 11042025.cpp
+++ 11042025.cpp
@@ -6,8 +6,24 @@ int main() {
     int N;
     cin >> N;
     
+    bool triangular = false;
+    for (int i = 1; i <= N; i++)
+    {
+        if (N == i * (i + 1) * (i + 2))
+        {
+            triangular = true;
+        }
+    }
+    if (triangular == true)
+    {
+        cout << "triangular";
 
-
+    }
+    else {
+        cout << "not triangular";
+    }        
+    
+    
 
 
     return 0;
---
+++ 18042025.cpp
@@ -0,0 +1,9 @@
+/*
+atividades práticas das aulas de ITP*/
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    
+}
