14546dd969b0a4efc8fafc1784af2ad94cd0a6fb
ozzi17 <oslifernandes@gmail.com>
2025-05-26 19:44:28 -0300

    Refactor power function and clean up commented code; adjust main function for clarity

--- maio2025/funcao.cpp
+++ maio2025/funcao.cpp
@@ -1,23 +1,67 @@
 #include <iostream>
 #include <string>
 using namespace std;
-/*
-string invertida(string strg){
-    string r = "";
-    for (int i = 0; i < strg.length(); ++i)
+/* //PROBLEMA 1
+float power(float B, int P){
+    float c = B;
+    while (P > 1)
     {
-        r = strg[i] + r;
+        B = B * c;
+        P--;
     }
-    
-    return r;
+    return B;
 }
 
 int main() {
+    float numero;
+    int indice;
+    cin >> numero >> indice;
 
-    string strg;
-    cin >> strg;
-    cout << invertida(strg);
+    cout << power(numero, indice);
+}
+
+*/
+/* //PROBLEMA 2
+int max(int vetor[], int tam){
+    int maior = vetor[0];
+    for (int i = 1; i < tam; i++)
+    {
+        if (maior < vetor[i])
+        {
+            maior = vetor[i];
+        }
+
+    }
+    return maior;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int vetor[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> vetor[i];
+    }
+    cout << max(vetor, n);
+
 }
     */
 
-
+int mdc(int a, int b) {
+  int maior = a;
+  int menor = b;
+  int temp;
+  if (b > a) {
+    temp = a;
+    maior = b;
+    menor = temp;
+  }
+  int n = maior % menor;
+  while (n != 0) {
+    b = a;
+    a = n;
+    n = b % a;
+  }
+  return n;
+}
