3fd1a92bda3b9ef2f3bfc4a8cb91d67a4ddd306c
ozzi17 <oslifernandes@gmail.com>
2025-05-29 08:57:02 -0300

    Implement division function and update main for user input handling

--- maio2025/mc1/div.cpp
+++ maio2025/mc1/div.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 using namespace std;
-int div(int number) {
-    int A, B, q, r;
+int absoluto(int x) { return (x < 0) ? x*-1 : x; }
 
-
-    return 0;
+int divisao(int a, int b) {
+  int count = 0;
+  a = absoluto(a);
+  b = absoluto(b);
+  while (a > 0) {
+    a = a - b;
+    count++;
+  }
+  return count;
 }
 
 int main() {
-    return 0;
+  int dividendo, divisor, sinaldividendo = 1, sinaldivisor = 1;
+  cin >> dividendo >> divisor;
+  if (dividendo < 0) {
+    sinaldividendo = -1;
+  }
+  if (divisor < 0) {
+    sinaldivisor = -1;
+  }
+  cout << divisao(dividendo, divisor) * sinaldividendo * sinaldivisor;
 }
