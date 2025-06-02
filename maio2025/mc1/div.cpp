eb4911bbba2a740add65cc681f4430239d401925
Osli <oslifernandes@gmail.com>
2025-06-02 13:56:28 -0300

    Update div.cpp

--- maio2025/mc1/div.cpp
+++ maio2025/mc1/div.cpp
@@ -3,24 +3,30 @@ using namespace std;
 int absoluto(int x) { return (x < 0) ? x*-1 : x; }
 
 int divisao(int a, int b) {
+  if (b == 0){
+    cout << "Não é possível dividir por 0" << endl;
+    return 0;
+  }
   int count = 0;
+  int sinaldividendo = 1, sinaldivisor = 1;
+  if (a < 0) {
+    sinaldividendo = -1;
+  }
+  if (b < 0) {
+    sinaldivisor = -1;
+  }
   a = absoluto(a);
   b = absoluto(b);
-  while (a > 0) {
+  while (a >= b) {
     a = a - b;
     count++;
   }
-  return count;
+  return count * sinaldividendo *sinaldivisor;
 }
 
 int main() {
-  int dividendo, divisor, sinaldividendo = 1, sinaldivisor = 1;
+  int dividendo, divisor;
   cin >> dividendo >> divisor;
-  if (dividendo < 0) {
-    sinaldividendo = -1;
-  }
-  if (divisor < 0) {
-    sinaldivisor = -1;
-  }
-  cout << divisao(dividendo, divisor) * sinaldividendo * sinaldivisor;
+  cout << divisao(dividendo, divisor);
+  return 0;
 }
